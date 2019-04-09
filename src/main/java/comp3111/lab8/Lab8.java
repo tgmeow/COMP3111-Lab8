8531ea4f08a5bba1ec7330fb7adce721ef69feaa
Tiger Mou <tgmeow16@gmail.com>
2019-04-09 21:12:16 +0800

    another change

--- src/main/java/comp3111/lab8/Lab8.java
+++ src/main/java/comp3111/lab8/Lab8.java
@@ -3,7 +3,7 @@ package comp3111.lab8;
 public class Lab8 {
 
   public static void main(String[] args) {
-    System.out.println("HELo LAB 8!!!!!!!!!!!!");
+    System.out.println("HELo LAB! 8!!!!!!!!!!!!");
     System.out.println("KIM WAS HERE LOL");
   }
 }
