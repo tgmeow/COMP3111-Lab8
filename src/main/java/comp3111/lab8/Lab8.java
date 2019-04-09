7b29a53b88dd6464997549c1305346af69ac1c9d
Tiger Mou <tgmeow16@gmail.com>
2019-04-09 21:03:04 +0800

    gonna fix this merge err

--- src/main/java/comp3111/lab8/Lab8.java
+++ src/main/java/comp3111/lab8/Lab8.java
@@ -3,6 +3,6 @@ package comp3111.lab8;
 public class Lab8 {
 
   public static void main(String[] args) {
-    System.out.println("HELLO LAB 8!!!!!!!!!!!!!!");
+    System.out.println("HELo LAB 8!!!!!!!!!!!!");
   }
 }
